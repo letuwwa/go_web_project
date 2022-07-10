18fa7160667dac431f8a29e66ba9ac4b18824e36
Litvinenka <letuwwa@gmail.com>
2022-07-10 21:43:23 +0400

    feat: token check

--- middleware/auth_middlewares.go
+++ middleware/auth_middlewares.go
@@ -1,13 +1,24 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/labstack/echo/v4"
+	"go_web_project/utils"
+	"net/http"
 )
 
 func TestMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		fmt.Println("middleware is active")
+		JWTToken := c.Request().Header["Token"]
+		if len(JWTToken) == 0 {
+			return c.JSON(http.StatusForbidden, utils.Response{Status: utils.JWTOperationError})
+		}
+		token, err := utils.ValidateJWT(JWTToken[0])
+		if err != nil {
+			return c.JSON(http.StatusForbidden, utils.Response{Status: err.Error()})
+		}
+		if token.Valid == false {
+			return c.JSON(http.StatusForbidden, utils.Response{Status: err.Error()})
+		}
 		return next(c)
 	}
 }
