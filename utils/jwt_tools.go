21f7b806f07ca4642a48d08778e8d06ac00d3e34
Litvinenka <letuwwa@gmail.com>
2022-07-10 20:04:26 +0400

    feat: ValidateJWT

--- utils/jwt_tools.go
+++ utils/jwt_tools.go
@@ -1,15 +1,24 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/golang-jwt/jwt"
 	"time"
 )
 
 func GenerateJWT(username string) (string, error) {
-	var mySigningKey = []byte(JWTSalt)
 	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 	claims["username"] = username
 	claims["exp"] = time.Now().Add(time.Hour * 3).Unix()
-	return token.SignedString(mySigningKey)
+	return token.SignedString([]byte(JWTSalt))
+}
+
+func ValidateJWT(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("there was an error in parsing token")
+		}
+		return []byte(JWTSalt), nil
+	})
 }
